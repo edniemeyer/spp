e923f06258d34c19eacb6a0d53151cd2f25e90ac
eduardo <edniemeyer@gmail.com>
2019-02-11 16:05:45 +1300

    Graph class created

---
+++ src/spp/Graph.java
@@ -0,0 +1,16 @@
+package spp;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * A Graph is a set of Nodes, so it needs an attribute representing this set and a method to add Nodes to it
+ */
+public class Graph {
+
+	private Set<Node> nodes = new HashSet<Node>();
+	
+	public void addNode(Node node) {
+		nodes.add(node);
+	}
+}
