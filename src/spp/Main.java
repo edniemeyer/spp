574288cae8f9e185838503d49c2bce36e349871d
eduardo <edniemeyer@gmail.com>
2019-02-11 18:24:31 +1300

    Created some input checking error handling

--- src/spp/Main.java
+++ src/spp/Main.java
@@ -1,12 +1,34 @@
 package spp;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
+		
+		ArrayList<String> possibleArguments = new ArrayList<String>();
+		possibleArguments.add("A");
+		possibleArguments.add("B");
+		possibleArguments.add("C");
+		possibleArguments.add("D");
+		possibleArguments.add("E");
+		possibleArguments.add("F");
+		possibleArguments.add("G");
+		possibleArguments.add("H");
+		possibleArguments.add("I");
+		possibleArguments.add("J");
 
+		if(args.length == 0)
+			throw new Exception("No input found");
+		
+		if (args.length != 2)
+			throw new Exception("You can only input 2 arguments");
+		
+		if (!possibleArguments.contains(args[0].toUpperCase()) || !possibleArguments.contains(args[1].toUpperCase()))
+			throw new Exception("Only inputs permitted are letters A to J");
+		
 		Node startNode = null, endNode = null;
 
 		Data data = new Data();
